10e0465b5df98cb58d3403244a7671252e0bc1a4
Eva Liu <evaxinerliu@gmail.com>
2023-02-09 12:09:31 -0800

    progress on save coin

---
+++ Assets/Scripts/HUD/CoinsCollectedText.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinsCollectedText : MonoBehaviour, IDataPersistence
+{
+    private float coinsCollected = 0;
+
+    private TextMeshProUGUI coinsCollectedText;
+
+    private void Awake() 
+    {
+        coinsCollectedText = this.GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start() 
+    {
+        // subscribe to events
+        GameManager._instance.onCoinCollected += OnCoinCollected;
+    }
+
+    public void LoadData(GameData data) 
+    {
+        coinsCollected = data.coinsCollected;
+    }
+
+    public void SaveData(GameData data)
+    {
+        // no data needs to be saved for this script
+    }
+
+    private void OnDestroy() 
+    {
+        // unsubscribe from events
+        GameManager._instance.onCoinCollected -= OnCoinCollected;
+    }
+
+    private void OnCoinCollected() 
+    {
+        coinsCollected++;
+    }
+
+    private void Update() 
+    {
+        coinsCollectedText.text = "" + coinsCollected;
+    }
+}
