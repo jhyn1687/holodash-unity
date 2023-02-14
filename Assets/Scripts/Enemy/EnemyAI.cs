4499d17b98052982eefa34c7bf6b46a332043e1d
Tony Yuan <Tonyyuan982@gmail.com>
2023-02-14 01:03:30 -0800

    enemy ai alert change

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -43,7 +43,7 @@ public class EnemyAI : MonoBehaviour {
     }
     void FixedUpdate()
     {
-        if (eb.currentState == EnemyBehavior.EnemyState.Patrol || eb.currentState == EnemyBehavior.EnemyState.Idle || eb.currentState == EnemyBehavior.EnemyState.Alert) {
+        if (eb.currentState == EnemyBehavior.EnemyState.Patrol || eb.currentState == EnemyBehavior.EnemyState.Idle) {
             if (frontGroundInfo.collider == null && backGroundInfo.collider == null) {
                 // can't patrol
                 canPatrol = false;
