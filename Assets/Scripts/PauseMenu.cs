dd05e1abd79497763529ba6f7bb28aedfeab44e4
kevin <lee1224@uw.edu>
2023-02-14 21:18:24 -0800

    menu and pausemenu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -40,4 +41,10 @@ public class PauseMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Quit");
     }
+
+     public void BacktoMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    
 }
